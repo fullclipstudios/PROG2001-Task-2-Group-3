31b41c80ab61b8fb317aa5146ca6b8b7c3ac522b
Zac-9898 <Zaccy_9898@hotmail.com>
2026-04-11 15:57:01 +0800

    Project 1 fixed
    Added imported asset, animations, player input scripts

---
+++ Assets/IsaacFolder/Scripts/SpiderController.cs
@@ -0,0 +1,85 @@
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpiderController : MonoBehaviour
+{
+    public float speed = 15f;
+    public float jumpForce = 5f;
+    public float keyRotationSpeed = 150f;
+    public float mouseRotationSpeed = 1f;
+
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementZ;
+    private float rotateKeys;
+    private float rotateMouse;
+
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Called by Player Input (Send Messages)
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementZ = movementVector.y;
+    }
+
+    void OnRotateKeys(InputValue value)
+    {
+        rotateKeys = value.Get<float>(); // -1 to 1
+    }
+
+    void OnRotateMouse(InputValue value)
+    {
+        rotateMouse = value.Get<float>(); // mouse delta
+    }
+
+    void OnJump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+        // Movement direction based on input
+        Vector3 movement = transform.forward * movementZ + transform.right * movementX;
+
+        if (movement.magnitude > 0.01f)
+        {
+            rb.velocity = new Vector3(
+                movement.normalized.x * speed,
+                rb.velocity.y, // preserve gravity
+                movement.normalized.z * speed
+            );
+        }
+        else
+        {
+            rb.velocity = new Vector3(
+                0f,
+                rb.velocity.y, // preserve gravity
+                0f
+            );
+        }
+
+        float keyRotation = rotateKeys * keyRotationSpeed * Time.fixedDeltaTime;
+        float mouseRotation = rotateMouse * mouseRotationSpeed;
+
+        // Rotation
+        transform.Rotate(Vector3.up * (keyRotation + mouseRotation));
+
+        float currentSpeed = new Vector3(rb.velocity.x, 0f, rb.velocity.z).magnitude;
+
+        if (currentSpeed < 0.05f)
+            currentSpeed = 0f;
+
+        animator.SetFloat("Speed", currentSpeed);
+    }
+}
