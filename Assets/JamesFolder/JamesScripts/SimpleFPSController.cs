e1d7ebef65b97d00c30fa336525b65f33db08f1c
fullclipstudios <jamesoliveris@yahoo.com.au>
2026-04-13 17:22:00 +1000

    added movement script prefab

---
+++ Assets/JamesFolder/JamesScripts/SimpleFPSController.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class SimpleFPSController : MonoBehaviour
+{
+    [Header("Speed")]
+
+    [Tooltip("Speed of the player")]
+    [SerializeField] private float speed = 5f;
+    
+    [Header("Jump & Gravity")]
+    [Tooltip("Player gravity")]
+    [SerializeField] private float gravity = -9.81f;
+    [Tooltip("Jump Height")]
+    [SerializeField] private float jumpHeight = 1.5f;
+
+    [Header("Sensitivity")]
+    [Tooltip("Mouse sens")]
+    [SerializeField] private float mouseSensitivity = 100f;
+
+    
+
+    [Header("Look clamping")]
+    [Tooltip("Max down look")]
+    [SerializeField] private float minLookAngle = -90f;
+    [Tooltip("Max up Look")]
+    [SerializeField] private float maxLookAngle = 90f; 
+
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private float yVelocity;
+    private float xRotation = 0f;
+
+
+     
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Look();
+        Move();
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, minLookAngle, maxLookAngle);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (controller.isGrounded && yVelocity < 0)
+        {
+            yVelocity = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            yVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        yVelocity += gravity * Time.deltaTime;
+
+        Vector3 velocity = move * speed;
+        velocity.y = yVelocity;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
