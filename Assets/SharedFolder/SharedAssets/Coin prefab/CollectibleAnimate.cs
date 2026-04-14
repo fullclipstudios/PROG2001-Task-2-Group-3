9b7b981a4160c0d0806f9de6f4784145fa80d96a
Zac-9898 <Zaccy_9898@hotmail.com>
2026-04-14 14:02:56 +0800

    corrected naming convention

--- Assets/SharedFolder/SharedAssets/Coin prefab/CollectibleAnimate.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class CommonCoinRotator : MonoBehaviour
-{
-    void Update() // Basic Z axis spin
-    {
-        transform.Rotate(new Vector3(0, 0, 30) * Time.deltaTime);
-    }
-}
